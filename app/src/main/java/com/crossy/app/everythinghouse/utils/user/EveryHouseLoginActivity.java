b723b0c07882bbb113ef50b7bab71e5f4d4c9d37
ljj <ljjcode@gmail.com>
2014-12-25 22:40:20 +0800

    登陆页面

--- app/src/main/java/com/crossy/app/everythinghouse/utils/user/EveryHouseLoginActivity.java
+++ app/src/main/java/com/crossy/app/everythinghouse/utils/user/EveryHouseLoginActivity.java
@@ -2,19 +2,62 @@ package com.crossy.app.everythinghouse.utils.user;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 import com.crossy.app.everythinghouse.R;
 import com.crossy.app.everythinghouse.utils.ViewUtil;
 
 public class EveryHouseLoginActivity extends Activity {
 
+    private BootstrapEditText editTextUsername;
+    private BootstrapEditText editTextPassword;
+    private BootstrapButton buttonLogin;
+    private TextView textViewRegister;
+
+    private String username;
+    private String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_every_house_login);
+
+        initView();
+
+        registerListener();
+
         initActionBar();
+    }
+
+    private void initView(){
+        editTextUsername = (BootstrapEditText)findViewById(R.id.editTextUsername);
+        editTextPassword = (BootstrapEditText)findViewById(R.id.editTextPassword);
+        buttonLogin = (BootstrapButton)findViewById(R.id.buttonLogin);
+        textViewRegister = (TextView)findViewById(R.id.textViewRegister);
+    }
+
+    private void registerListener(){
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = editTextUsername.getText().toString();
+                password = editTextPassword.getText().toString();
+
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(EveryHouseLoginActivity.this);
+
+                dialog.show();
+            }
+        });
     }
 
     private void initActionBar(){
--- app/src/main/java/com/crossy/app/everythinghouse/utils/user/EveryHouseUserActivity.java
+++ app/src/main/java/com/crossy/app/everythinghouse/utils/user/EveryHouseUserActivity.java
@@ -2,21 +2,43 @@ package com.crossy.app.everythinghouse.utils.user;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.crossy.app.everythinghouse.R;
 import com.crossy.app.everythinghouse.utils.ViewUtil;
 
 public class EveryHouseUserActivity extends Activity {
 
+    private BootstrapButton buttonGoLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_every_house_user);
+
+        initView();
+
+        registerListener();
+
         initActionBar();
+    }
+
+    private void initView(){
+        buttonGoLogin = (BootstrapButton)findViewById(R.id.buttonGoLogin);
+    }
+
+    private void registerListener(){
+        buttonGoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(EveryHouseUserActivity.this,EveryHouseLoginActivity.class));
+            }
+        });
     }
 
     private void initActionBar(){
