9cfc33052b2e4d2359f33685b49236f989ce6fe8
ljj <ljjcode@gmail.com>
2014-12-25 20:34:48 +0800

    绑定控件

--- app/src/main/java/com/crossy/app/everythinghouse/acts/EveryHouseActsActivity.java
+++ app/src/main/java/com/crossy/app/everythinghouse/acts/EveryHouseActsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.crossy.app.everythinghouse.R;
 import com.crossy.app.everythinghouse.utils.ViewUtil;
@@ -13,10 +14,18 @@ import com.crossy.app.everythinghouse.utils.launch.TabHostActivity;
 
 public class EveryHouseActsActivity extends Activity {
 
+    private LinearLayout layoutAct1;
+    private LinearLayout layoutAct2;
+    private LinearLayout layoutAct3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_every_house_acts);
+
+        initView();
+
+        registerListener();
 
         initActionBar();
     }
@@ -25,6 +34,33 @@ public class EveryHouseActsActivity extends Activity {
     protected void onResume(){
         super.onResume();
         initActionBar();
+    }
+
+    private void initView(){
+        layoutAct1 = (LinearLayout)findViewById(R.id.layoutAct1);
+        layoutAct2 = (LinearLayout)findViewById(R.id.layoutAct2);
+        layoutAct3 = (LinearLayout)findViewById(R.id.layoutAct3);
+    }
+
+    private void registerListener(){
+        layoutAct1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        layoutAct2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        layoutAct3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void initActionBar(){
--- app/src/main/java/com/crossy/app/everythinghouse/orders/send/EveryHouseOrderSendActivity.java
+++ app/src/main/java/com/crossy/app/everythinghouse/orders/send/EveryHouseOrderSendActivity.java
@@ -7,18 +7,57 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 import com.crossy.app.everythinghouse.R;
 import com.crossy.app.everythinghouse.utils.ViewUtil;
 
 public class EveryHouseOrderSendActivity extends Activity {
+
+    private Button buttonPickTime;
+    private BootstrapEditText editTextLocation;
+    private BootstrapEditText editTextMoney;
+    private BootstrapEditText editTextContent;
+    private CheckBox checkBoxAnonymous;
+
+    private String time;
+    private String location;
+    private String money;
+    private String content;
+    private boolean isAnonymous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_every_house_order_send);
 
+        initView();
+
+        registerListener();
+
         initActionBar();
+    }
+
+    private void initView(){
+        buttonPickTime = (Button)findViewById(R.id.buttonPickTime);
+        editTextLocation = (BootstrapEditText)findViewById(R.id.editTextLocation);
+        editTextMoney = (BootstrapEditText)findViewById(R.id.editTextMoney);
+        editTextContent = (BootstrapEditText)findViewById(R.id.editTextContent);
+        checkBoxAnonymous = (CheckBox)findViewById(R.id.checkboxAnonymous);
+
+        buttonPickTime.setText("0000-00-00 00:00:00(点击修改)");
+    }
+
+    private void registerListener(){
+        buttonPickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void initActionBar(){
